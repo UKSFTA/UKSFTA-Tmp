b4111f46206ca127941d5b98222e75031f41a6a0
Matthew Barker <matthewbarker@librem.one>
2026-02-10 17:12:36 +0000

    fix: update PBO prefixes to universal standard z\uksfta\addons\main

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -1,11 +1,11 @@
 class CfgMods {
     author = "UKSF Taskforce Alpha Team";
-    logo = "z\uksfta\tmp\data\icon_128_ca.paa";
-    logoOver = "z\uksfta\tmp\data\icon_128_highlight_ca.paa";
-    logoSmall = "z\uksfta\tmp\data\icon_64_ca.paa";
+    logo = "z\uksfta\addons\main\data\icon_128_ca.paa";
+    logoOver = "z\uksfta\addons\main\data\icon_128_highlight_ca.paa";
+    logoSmall = "z\uksfta\addons\main\data\icon_64_ca.paa";
     name = "UKSF Taskforce Alpha Mods";
     overview = "UKSF Taskforce Alpha";
-    picture = "z\uksfta\tmp\data\title_co.paa";
+    picture = "z\uksfta\addons\main\data\title_co.paa";
     tooltip = "UKSFTA";
     tooltipOwned = "UKSF Taskforce Alpha";
 };
