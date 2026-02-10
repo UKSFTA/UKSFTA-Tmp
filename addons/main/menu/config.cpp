00029f6ab249feb158a2df6c6d48ae44ef90c670
Matthew Barker <matthewbarker@librem.one>
2026-02-10 20:50:01 +0000

    fix: standardize VFS paths to z\uksfta\addons and ensure Steam compatibility

--- addons/main/menu/config.cpp
+++ addons/main/menu/config.cpp
@@ -126,7 +126,7 @@ class RscDisplayMain: RscStandardDisplay
 		};
 		class logo: RscActivePicture
 		{
-			text = "z\uksfta\menu\assets\icon_ca.paa";
+			text = "z\uksfta\addons\menu\assets\icon_ca.paa";
 			url = "";
 			tooltip = "";
 			onButtonClick = "";
@@ -196,7 +196,7 @@ class CfgMissions
 	{
 		class mymainmenu
 		{
-			directory = "z\uksfta\menu\mainmenu.vr";
+			directory = "z\uksfta\addons\menu\mainmenu.vr";
 		};
 	};
 };
--- addons/radio_cba/XEH_PREP.hpp
+++ addons/radio_cba/XEH_PREP.hpp
@@ -1,7 +1,7 @@
-_fnc_addRadioTrackActions = compile preprocessFileLineNumbers "\z\uksfta\radio_cba\functions\fnc_addRadioTrackActions.sqf";
-_fnc_canPlayRadio = compile preprocessFileLineNumbers "\z\uksfta\radio_cba\functions\fnc_canPlayRadio.sqf";
-_fnc_canStopRadio = compile preprocessFileLineNumbers "\z\uksfta\radio_cba\functions\fnc_canStopRadio.sqf";
-_fnc_changeVolume = compile preprocessFileLineNumbers "\z\uksfta\radio_cba\functions\fnc_changeVolume.sqf";
-_fnc_getRadioTracks = compile preprocessFileLineNumbers "\z\uksfta\radio_cba\functions\fnc_getRadioTracks.sqf";
-_fnc_playRadio = compile preprocessFileLineNumbers "\z\uksfta\radio_cba\functions\fnc_playRadio.sqf";
-_fnc_stopRadio = compile preprocessFileLineNumbers "\z\uksfta\radio_cba\functions\fnc_stopRadio.sqf";
+_fnc_addRadioTrackActions = compile preprocessFileLineNumbers "\z\uksfta\addons\radio_cba\functions\fnc_addRadioTrackActions.sqf";
+_fnc_canPlayRadio = compile preprocessFileLineNumbers "\z\uksfta\addons\radio_cba\functions\fnc_canPlayRadio.sqf";
+_fnc_canStopRadio = compile preprocessFileLineNumbers "\z\uksfta\addons\radio_cba\functions\fnc_canStopRadio.sqf";
+_fnc_changeVolume = compile preprocessFileLineNumbers "\z\uksfta\addons\radio_cba\functions\fnc_changeVolume.sqf";
+_fnc_getRadioTracks = compile preprocessFileLineNumbers "\z\uksfta\addons\radio_cba\functions\fnc_getRadioTracks.sqf";
+_fnc_playRadio = compile preprocessFileLineNumbers "\z\uksfta\addons\radio_cba\functions\fnc_playRadio.sqf";
+_fnc_stopRadio = compile preprocessFileLineNumbers "\z\uksfta\addons\radio_cba\functions\fnc_stopRadio.sqf";
